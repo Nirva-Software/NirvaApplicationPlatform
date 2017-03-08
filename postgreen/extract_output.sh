3b808729f75bbd5507ca09854c78cd2d7a5a29f6
lionel <lionel@lionelmartin-LYON>
2017-03-08 15:11:54 +0100

    Fix the dummy ECHO variable. Use a echo command instead

--- postgreen/extract_output.sh
+++ postgreen/extract_output.sh
@@ -2,9 +2,9 @@
 
 # Load parameters
 if [ $# -ne 3 ]; then
-	${ECHO} "Usage: extract_output.sh <creation_date_to_extract> <What to find in the xml file> <create tar file>" 1>&2
-	${ECHO} 'For example: extract_output.sh 20170228 "<name>MARCO POLO FOODS</name>" "../20170228.tgz"' 1>&2
-	${ECHO} 'Another example: extract_output.sh 2017/01/20170115 "<login>marco_polo_foods:polling</login>" "/home/nirva/temp/20170115-marcopolo.tgz"' 1>&2
+	echo "Usage: extract_output.sh <creation_date_to_extract> <What to find in the xml file> <create tar file>" 1>&2
+	echo 'For example: extract_output.sh 20170228 "<name>MARCO POLO FOODS</name>" "../20170228.tgz"' 1>&2
+	echo 'Another example: extract_output.sh 2017/01/20170115 "<login>marco_polo_foods:polling</login>" "/home/nirva/temp/20170115-marcopolo.tgz"' 1>&2
 	exit 1
 fi
 
@@ -13,7 +13,7 @@ FIND_STR="$2"
 FILEPATH="$3"
 
 if [ -e "$FILEPATH" ]; then
-	${ECHO} "File '$FILEPATH' already exist!" 1>&2
+	echo "File '$FILEPATH' already exist!" 1>&2
 	exit 1
 fi
 
