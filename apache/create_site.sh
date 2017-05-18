2345bbb93c3947a75f718a2b30fcdd568591b0c5
lionel <lionel@lionelmartin-LYON>
2017-05-18 16:00:29 +0200

    PostGreen Apache sites: add a mode for test or prod, clean code to manage apache static pages internally in the create_site.sh script and the template is now harcoded in the script add also a script to manage letsEncrypt ssl certificates more easily

--- apache/create_site.sh
+++ apache/create_site.sh
@@ -12,39 +12,62 @@ if [[ $EUID -ne 0 ]]; then
    die "This script must be run as root"
 fi
 
-# ./create_site.sh <provider_code> domain site_template certificate_file certificate_key certificate_CA applicationName
-[ "$#" -eq 7 ] || die "Usage: ./create_site.sh <provider_code> domain site_template certificate_file certificate_key certificate_CA applicationName"
+# ./create_site.sh mode <provider_code> domain certificate_file certificate_key certificate_CA applicationName
+[ "$#" -eq 7 ] || die "Usage: ./create_site.sh test|prod <provider_code> domain certificate_file certificate_key certificate_CA applicationName"
 
-CODE=$1
-DOMAIN=$2
-TEMPLATE=$3
+MODE=$1
+if [ "$MODE" != "prod" ]; then
+	MODE=test
+fi
+CODE=$2
+DOMAIN=$3
 CERT=$4
 CERT_KEY=$5
 CERT_CA=$6
 APPLICATION_NAME=$7
 
 if [ ! -f "$CERT" ]; then
-	CERT="/etc/apache2/ssl/$CERT"
+	if [ -f "/etc/apache2/ssl/$CERT" ]; then
+		CERT="/etc/apache2/ssl/$CERT"
+	else
+		CERT="/etc/apache2/letsencrypt/certs/$CERT"
+	fi
 fi
 if [ ! -f "$CERT_KEY" ]; then
-	CERT_KEY="/etc/apache2/ssl/$CERT_KEY"
+	if [ -f "/etc/apache2/ssl/$CERT_KEY" ]; then
+		CERT_KEY="/etc/apache2/ssl/$CERT_KEY"
+	else
+		CERT_KEY="/etc/apache2/letsencrypt/certs/$CERT_KEY"
+	fi
 fi
 if [ ! -f "$CERT_CA" ]; then
-	CERT_CA="/etc/apache2/ssl/$CERT_CA"
+	if [ -f "/etc/apache2/ssl/$CERT_CA" ]; then
+		CERT_CA="/etc/apache2/ssl/$CERT_CA"
+	else
+		CERT_CA="/etc/apache2/letsencrypt/certs/$CERT_CA"
+	fi
 fi
 
 OLD_REP=`pwd`
 
 # Generate static pages for Apache
-./generate_apache_pages.sh "$CODE" "$APPLICATION_NAME" "$DOMAIN"
+cd /var/www
+if [ -d "${CODE}.conf" ]; then
+  echo "Removing Old directory /var/www/${CODE}.conf"
+  [ -d "${CODE}.conf.bak" ] && rm -R "${CODE}.conf.bak"
+  mv "${CODE}.conf" "${CODE}.conf.bak"
+fi
+cp -R template ${CODE}.conf
+sed -i "s/\\\${APPLICATION_NAME}/${APPLICATION_NAME}/g" ${CODE}.conf/error.htm
 
+# Generate apache site from template
 cd /etc/apache2/sites-available/
 
-cp $TEMPLATE $CODE
-sed -i "s/server_name/$DOMAIN/g" $CODE
-sed -i "s/\/var\/www\/site_folder/\/var\/www\/$CODE/g" $CODE
-sed -i "s~SSLCertificateFile /etc/apache2/ssl/certificate.crt~SSLCertificateFile $CERT~" $CODE
-sed -i "s~SSLCertificateKeyFile /etc/apache2/ssl/certificate.key~SSLCertificateKeyFile $CERT_KEY~" $CODE
-sed -i "s~SSLCACertificateFile /etc/apache2/ssl/CAcertificate.pem~SSLCACertificateFile $CERT_CA~" $CODE
+cp "apache_template_${MODE}.conf" ${CODE}.conf
+sed -i "s/server_name/$DOMAIN/g" ${CODE}.conf
+sed -i "s/\/var\/www\/site_folder/\/var\/www\/${CODE}.conf/g" ${CODE}.conf
+sed -i "s~SSLCertificateFile /etc/apache2/ssl/certificate.crt~SSLCertificateFile $CERT~" ${CODE}.conf
+sed -i "s~SSLCertificateKeyFile /etc/apache2/ssl/certificate.key~SSLCertificateKeyFile $CERT_KEY~" ${CODE}.conf
+sed -i "s~SSLCACertificateFile /etc/apache2/ssl/CAcertificate.pem~SSLCACertificateFile $CERT_CA~" ${CODE}.conf
 
 cd $OLD_REP
---
+++ apache/create_site_letsencrypt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+die () {
+        echo -e >&2 "$@"
+        exit 1
+}
+
+set -e # Exits at first failure
+set -u # Exits when a non initialized variable is used
+
+if [[ $EUID -ne 0 ]]; then
+   die "This script must be run as root"
+fi
+
+# ./create_site_letsencrypt.sh mode <provider_code> domain applicationName letsencrypt_domain
+[ "$#" -eq 4 ] || [ "$#" -eq 5 ] || die "Usage: ./create_site_letsencrypt.sh test|prod <provider_code> domain applicationName\nor ./create_site_letsencrypt.sh test|prod <provider_code> domain applicationName letsencrypt_domain\nif the letsencrypt configuration is common with another sub-domain."
+
+MODE=$1
+if [ "$MODE" != "prod" ]; then
+	MODE=test
+fi
+CODE=$2
+DOMAIN=$3
+APPLICATION_NAME=$4
+CERT_DOMAIN=$5
+[[ -z "$CERT_DOMAIN" ]] && CERT_DOMAIN="$DOMAIN"
+
+./create_site.sh "$MODE" "$CODE" "$DOMAIN" "$CERT_DOMAIN/fullchain.pem" "$CERT_DOMAIN/privkey.pem" "$CERT_DOMAIN/cert.pem" "$APPLICATION_NAME"
